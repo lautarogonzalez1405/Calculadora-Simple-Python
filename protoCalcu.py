c679a31e6de21ab8fc389104b2b8b2058cc6ceb9
Lautaro Gonzalez <lautarogonzalez1405@gmail.com>
2026-01-08 16:22:54 -0300

    Feature: Posibilidad de revisar el historial de operaciones

--- protoCalcu.py
+++ protoCalcu.py
@@ -4,7 +4,11 @@ print('Nota: Si desea calcular el porcentaje, solo debe ingresar el numero segui
 print('Por favor, ingresa la operación que deseas realizar en el formato: número1 operador número2')
 print('Por ejemplo: 3 + 4 o 10 / 2')
 
+
+
 def calculadora():
+    
+    historial = []
 
     operadores = {
         '+': lambda a, b: a + b,
@@ -20,6 +24,12 @@ def calculadora():
         if operacion.lower() == 'salir':
             print("Gracias por usar la calculadora, hasta luego")
             break
+
+        if operacion.lower() == 'historial':
+            print('Mostrando el historial de operaciones: ')
+            for o in historial:
+                print(o)
+            continue
 
         try:
             partes = operacion.split()
@@ -37,6 +47,7 @@ def calculadora():
                 print("Error: no se puede dividir entre cero")
             else:
                 print(f"Resultado: {resultado}")
+                historial.append(f'{operacion} = {resultado}')
 
         except ValueError:
             print("Error: debes ingresar numeros validos")
